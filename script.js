926c2681e82c6b78d410eee8561241e679fc00cc
Aniketsomkuwar <aniketsomkuwar1101@gmail.com>
2021-12-16 17:23:01 +0530

    added files

---
+++ script.js
@@ -0,0 +1,19 @@
+const details = {
+  basics: {
+    firstname: null,
+    lastname: null,
+
+    email: null,
+    phone: null,
+
+    summary: null,
+    location: {
+      postalCode: null,
+      city: null,
+      state: null,
+    },
+  },
+};
+
+
+console.log(details.basics.lastname);
