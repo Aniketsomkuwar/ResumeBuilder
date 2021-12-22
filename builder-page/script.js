e419f960ad02bce4a542ef1e692bee6d1356e27e
Divyani247 <divyanihirulkar247@gmail.com>
2021-12-22 18:14:06 +0530

    files added

--- builder-page/script.js
+++ builder-page/script.js
@@ -14,6 +14,34 @@ const details = {
     },
   },
 };
-
-
 console.log(details.basics.lastname);
+
+var visiblediv=0;
+function showdiv()
+{
+  $(".more").hide();
+  $(".more:eq("+ visiblediv +")").show();
+}
+showdiv()
+function shownext()
+{
+  if(visiblediv==$(".more").length-1)
+  {
+    visiblediv=0;
+  }
+  else{
+    visiblediv ++;
+  }
+  showdiv();
+}
+function showprev()
+{
+  if (visiblediv==0)
+  {
+    visiblediv=$(".more").length-1;
+  }
+  else{
+    visiblediv--;
+  }
+  showdiv();
+}
